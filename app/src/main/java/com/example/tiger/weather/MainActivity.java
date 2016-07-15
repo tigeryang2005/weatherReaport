f60437cf747ce452bbc06a371d61383945c1cb07
yangyin <yangyin@inad.com>
2016-07-15 11:07:51 +0800

    add net request test

--- app/src/main/java/com/example/tiger/weather/MainActivity.java
+++ app/src/main/java/com/example/tiger/weather/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tiger.weather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.tiger.weather.util.HttpCallbackListener;
+import com.example.tiger.weather.util.HttpUtil;
 import com.orhanobut.logger.Logger;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int i = 0;
-        Logger.d(i);
+        HttpUtil.sendHttpRequest("http://www.weather.com.cn/data/list3/city.xml", new HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+                Logger.d(response);
+                String[] strings = response.split(",");
+                //Logger.d(strings);
+                for (String s : strings){
+                    Logger.d(s);
+                    String[] strings1 = s.split("\\|");//“.”和“|”都是转义字符，必须得加"\";
+                    for (String s1 : strings1){
+                        Logger.d(s1);
+                    }
+//                    Logger.d(strings1);
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Logger.d(e);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/tiger/weather/db/WrDb.java
@@ -0,0 +1,8 @@
+package com.example.tiger.weather.db;
+
+/**
+ * Created by tiger on 16/7/15.
+ */
+public class WrDb {
+
+}
